cd03ae0f7ebed7c5aedcbf0246b7e8672e61cbf5
djonik08@gmail.com <djonik08@gmail.com@58d5e0b1-e263-14c6-e8d8-15f8e3f31162>
2012-04-06 20:52:42 +0000

    Committing compAndRun.sh

--- trunk/fasttrack/compAndRun.sh
+++ trunk/fasttrack/compAndRun.sh
@@ -6,6 +6,8 @@ clear
 rrrun -tool=FT test.Test > rrout
 mkdir classes/test
 cp test/tmp/Test.class classes/test/
+cp test/tmp/ShadowThread.class classes/rr/state/
+cp test/tmp/FastTrackTool.class classes/tools/fasttrack
 echo "===================================="
 echo "===================================="
 echo "===================================="
