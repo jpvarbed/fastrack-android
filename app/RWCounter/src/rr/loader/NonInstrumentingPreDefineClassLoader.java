506c212acefc6cd088f74fd681483fe389d0fd79
jpvarbed <jpvarbed@gmail.com>
2012-04-13 22:20:22 +0000

    making my branch so dan doesnt get hurt

--- app/RWCounter/src/rr/loader/NonInstrumentingPreDefineClassLoader.java
+++
@@ -1,9 +0,0 @@
-package rr.loader;
-
-import rr.state.agent.DefineClassListener;
-
-public class NonInstrumentingPreDefineClassLoader implements DefineClassListener {
-	public byte[] define(ClassLoader definingLoader, final String name, final byte[] bytes)  {
-		return bytes;
-	}
-}
